b6aa9f496d7011cbee82580f474d5fcc2eb5b3bd
Florian Reznicek <floreznicek@hotmail.com>
2014-11-03 20:12:54 +0100

    prototype almost done

--- devbuild0.1/Assets/Scripts/Objects/Player/Play_AntibodyCannon.cs
+++ devbuild0.1/Assets/Scripts/Objects/Player/Play_AntibodyCannon.cs
@@ -13,13 +13,16 @@ public class Play_AntibodyCannon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if(GameObject.Find("Patho_GenericPathogen")==null) Camera.main.GetComponent<Camera_Follow>().Follow(GameObject.Find("Screen1"));
+		
 		if(gameObject.GetComponent<Play_Behaviour>().selected)
 		{
 			Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 			
-			if (Input.GetMouseButtonDown(0))
+			if (Input.GetMouseButtonDown(0) && Time.timeScale!=0)
 			{
 				anim.SetTrigger("Shoot");
 				GameObject clone = (GameObject) Instantiate(Resources.Load("Prefabs/Objects/Player/PLay_Antibody"), transform.position+transform.right, transform.rotation);
---
+++ devbuild0.1/Assets/guiProceed.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guiProceed : MonoBehaviour {
+	
+	public GameObject Player;
+	
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDown()
+	{
+		Camera.main.GetComponent<Camera_Follow>().Follow(Player);
+		if(Player.name == "Play_AntibodyCannon" || Player.name == "Play_Fress") Time.timeScale = 1;
+	}
+}
