3673dae3ab00a9064d528b68289ef7490fb78298
Josua Kucher <josua_kucher@yahoo.de>
2014-11-11 10:58:13 +0100

    Update 11.11.14

---
+++ devbuild0.1/Assets/Scripts/Classes/Converter_SVG.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PolygonCollections
+{
+	public class Converter_SVG  
+	{
+		protected List<Polygon> Polygons;
+		// Directory of the File to read
+		string sPath=@"";
+		// Filename
+		string sFilename="";
+		// Contents of the File
+		string sFileText;
+		string sPointString="";
+		string sSubstring="points=";
+
+		List<int> iIndexlist;
+
+		public Converter_SVG()
+		{
+
+		}
+		List<Polygon> ConvertSVG()
+		{
+			return Polygons;
+		}
+		void readFile()
+		{
+			string text = System.IO.File.ReadAllText(sPath+sFilename);
+		}
+		void parseText()
+		{
+			for(int i=0; i<sFileText.Length; i++)
+			{
+				if(sFileText.IndexOf(sSubstring,i)>=i)
+				{
+					iIndexlist.Add(sFileText.IndexOf(sSubstring,i));
+					i=sFileText.IndexOf(sSubstring,i);
+				}
+			}
+		}
+	}
+}
---
+++ devbuild0.1/Assets/Scripts/Classes/Polygon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PolygonCollections
+{
+	public class Polygon
+	{
+		protected Vector2[] Coordinates;
+
+		public Polygon(Vector2[] Coordinates)
+		{
+			this.Coordinates = Coordinates;
+		}
+
+		public Vector2[] GetCoordinates()
+		{
+			return Coordinates;
+		}
+		public Vector2 GetCoordinate(int i)
+		{
+			return Coordinates[i];
+		}
+	}
+}
