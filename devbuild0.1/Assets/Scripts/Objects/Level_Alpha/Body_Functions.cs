076eea23fce68c512d42f3e3eb789d033400614f
Josua Kucher <josua_kucher@yahoo.de>
2014-11-25 17:01:47 +0100

    Bodyfunctions and walls

---
+++ devbuild0.1/Assets/Scripts/Objects/Level_Alpha/Body_Functions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Body_Functions : MonoBehaviour 
+{
+	public int iRest=1;
+	public float fHealthDarm=100;
+	public float fHealthHerz=100;
+	public float fHealthLunge=100;
+
+	public float fTickSpeed=1;
+	float fLastTick;
+
+	void Start()
+	{
+		fLastTick=Time.time;
+	}
+
+	void Update()
+	{
+		RestBody();
+	}
+
+	private void RestBody ()
+	{
+		if(Time.time > fLastTick+fTickSpeed)
+		{
+			if(fHealthDarm<100)fHealthDarm+=iRest;
+			if(fHealthHerz<100)fHealthHerz+=iRest;
+			if(fHealthLunge<100)fHealthLunge+=iRest;
+		}
+	}
+}
