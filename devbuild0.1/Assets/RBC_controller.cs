8f6a571c745ecb7c9d8048e4579e4c46ba2cb017
Florian Reznicek <floreznicek@hotmail.com>
2014-12-01 15:20:53 +0100

    everything is awesome, when your merging your dream

---
+++ devbuild0.1/Assets/RBC_controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RBC_controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CheckDistance", 2, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void CheckDistance()
+	{
+		print(Vector2.Distance(transform.position, Camera.main.transform.position));
+		if(Vector2.Distance(transform.position, Camera.main.transform.position)>10) 
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+}
