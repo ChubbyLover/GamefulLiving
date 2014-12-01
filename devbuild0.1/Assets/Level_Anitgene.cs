e3b181223a749e479bdb1c6aa8feaf2e9cb44832
Josua Kucher <josua_kucher@yahoo.de>
2014-12-01 15:01:13 +0100

    New Gui, antigene Spawner, and more magic ponies

---
+++ devbuild0.1/Assets/Level_Anitgene.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level_Anitgene : MonoBehaviour {
+
+	Sprite[] Antigene;
+	
+	public float TimeStamp;
+	public float Livetime; 
+	public int iAmmountOfAntigenes=6;
+	// Use this for initialization
+	void Start () 
+	{
+		Object[] AntigeneBuffer = Resources.LoadAll("Sprites/Objects/Player/Aura/Antigene");
+
+		Antigene = new Sprite[iAmmountOfAntigenes];
+		int iUnevenNumbers =0;
+		for(int i = 0; i<iAmmountOfAntigenes;i++)
+		{
+			if(i%2==1) 
+			{
+				iUnevenNumbers++;
+				Antigene[i-iUnevenNumbers]=AntigeneBuffer[i] as Sprite;
+			}
+		}
+		float TimeStamp= Time.time;
+		gameObject.GetComponent<SpriteRenderer>().sprite = Antigene[Random.Range(0,Antigene.Length)] as Sprite;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Time.time>TimeStamp+Livetime) Destroy(gameObject);
+	}
+}
---
+++ devbuild0.1/Assets/Level_AnitgeneSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level_AnitgeneSpawner : MonoBehaviour {
+
+	public float Tickspeed=1;
+	float LastTick;
+
+	// Use this for initialization
+	void Start () 
+	{
+		LastTick=Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Time.time>LastTick+Tickspeed)
+		{
+			GameObject Anitgene = Instantiate (Resources.Load("Prefabs/Objects/Player/Antigene/Antigene"),transform.position, transform.rotation) as GameObject;
+			Anitgene.GetComponent<Level_Anitgene>().TimeStamp=Time.time;
+			LastTick=Time.time;
+		}
+	}
+}
